cba886822fc3a2af80ac6fae8bff0b67aecbaf73
Trinhkien94 <kieict.94@gmail.com>
2019-09-23 00:14:10 +0700

    500 view

--- test.py
+++ test.py
@@ -12,18 +12,29 @@ from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC
-driver = webdriver.Chrome()
-driver.get("https://vietnam-trader.com")
-# driver.implicitly_wait(30)
-delay = 10 # seconds
-try:
-    myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'app-hot-news .table tbody tr:nth-child(2)')))
-    javaScript = "$('app-hot-news .table tbody tr:nth-child(2)').click()"
-    driver.execute_script(javaScript)
-    print("Page is ready!")
-except Exception:
-    print("Loading took too much time!")
+import random
+import time
+import concurrent.futures
+driverLocation = './chromedriver' 
+with concurrent.futures.ThreadPoolExecutor(max_workers=500) as executor:
+    for i in range(0,500):
+        # executor.map(self.getWebContent, (url,))
+        driver = webdriver.Chrome(driverLocation)
+        driver.get("https://vietnam-trader.com")
+        # driver.implicitly_wait(30)
+        delay = 10 # seconds
+        try:
+            myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'app-hot-news .table tbody tr:nth-child(2)')))
+            javaScript = "$('app-newest-article .article-wrawper:nth-child("+str(random.randint(1,9))+")').click()"
+            driver.execute_script(javaScript)
+            print("Page is ready!")
+        except Exception:
+            print("Loading took too much time!")
+        myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'app-hot-news-side .card-body:nth-child(2)')))
+        javaScript = "$('html, body').animate({scrollTop: $('app-footer').offset().top}, 1000)"
+        driver.execute_script(javaScript)
+        # time.sleep(60)
+        driver.quit()
 # driver.find_element_by_css_selector('app-hot-news .table tbody tr:nth-child(2)').click()
 # driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='library_books'])[1]/following::p[1]").click()
 # driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='access_time'])[1]/following::h4[1]").click()
-input("Enter for continue:")
